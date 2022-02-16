73ec77e80756124ac8356e6d184fe3c40e3d7c63
RedFox354 <reznikomy@mail.ru>
2022-02-16 15:26:55 +0000

    last

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ def index():
 def process_data():
     if request.method == "POST":
         data = request.get_json()
+        print(data)
         conn = get_db_connection()
         cur = conn.cursor()
         data = cur.execute('INSERT INTO data (isAutomated, appName, appVersion, cookieEnabled, geolocation, platform, userAgent, javaEnabled, Height, Width, OutHeight, OutWidth, Opener, evalBrowser) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)',(str(data[0]),str(data[1]),str(data[2]),str(data[3]),str(data[4]),str(data[5]),str(data[6]),str(data[7]),str(data[8]),str(data[9]),str(data[10]),str(data[11]),str(data[12]),str(data[13])))
