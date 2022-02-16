654361474425779f730a8da468e8605d7c0a4bdf
RedFox354 <reznikomy@mail.ru>
2022-02-16 14:59:41 +0000

    see automated at html page

--- init_db.py
+++ init_db.py
@@ -8,11 +8,11 @@ with open('schema.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO data (isautomated, appName, appVersion, cookieEnabled, geolocation, platform, userAgent, javaEnabled, Height, Width, OutHeight, OutWidth, Opener, evalBrowser) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+cur.execute("INSERT INTO data (isAutomated, appName, appVersion, cookieEnabled, geolocation, platform, userAgent, javaEnabled, Height, Width, OutHeight, OutWidth, Opener, evalBrowser) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
             ('nonautomated', 'TestApp', 'TestVersion', 'true', 'testlocation', 'testplatform', 'testagent', 'true', '1000', '500', '1100', '600', 'testopener', '5')
             )
 
-cur.execute("INSERT INTO data (isautomated, appName, appVersion, cookieEnabled, geolocation, platform, userAgent, javaEnabled, Height, Width, OutHeight, OutWidth, Opener, evalBrowser) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+cur.execute("INSERT INTO data (isAutomated, appName, appVersion, cookieEnabled, geolocation, platform, userAgent, javaEnabled, Height, Width, OutHeight, OutWidth, Opener, evalBrowser) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
             ('automated', 'TestApp2', 'TestVersion2', 'true2', 'testlocation2', 'testplatform2', 'testagent2', 'true2', '1002', '502', '1102', '602', 'testopener2', '6')
             )
 
