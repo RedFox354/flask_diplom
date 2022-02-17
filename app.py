24675f55f29cc461ea14833e86cf68a669c3c7fb
RedFox354 <reznikomy@mail.ru>
2022-02-17 09:19:59 +0000

    "automated" fixed

--- app.py
+++ app.py
@@ -18,15 +18,14 @@ def index():
 
 @app.route('/', methods=['POST'])
 def process_data():
-    if request.method == "POST":
-        data = request.get_json()
-        print(data)
-        conn = get_db_connection()
-        cur = conn.cursor()
-        data = cur.execute('INSERT INTO data (isAutomated, appName, appVersion, cookieEnabled, geolocation, platform, userAgent, javaEnabled, Height, Width, OutHeight, OutWidth, Opener, evalBrowser) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)',(str(data[0]),str(data[1]),str(data[2]),str(data[3]),str(data[4]),str(data[5]),str(data[6]),str(data[7]),str(data[8]),str(data[9]),str(data[10]),str(data[11]),str(data[12]),str(data[13])))
-        conn.commit()
-        conn.close()
-        return jsonify({'result': 'ok'})
+    data = request.get_json()
+    conn = get_db_connection()
+    cur = conn.cursor()
+    data = cur.execute('INSERT INTO data (isAutomated, appName, appVersion, cookieEnabled, geolocation, platform, userAgent, javaEnabled, Height, Width, OutHeight, OutWidth, Opener, evalBrowser) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
+                    (str(data[0]),str(data[1]),str(data[2]),str(data[3]),str(data[4]),str(data[5]),str(data[6]),str(data[7]),str(data[8]),str(data[9]),str(data[10]),str(data[11]),str(data[12]),str(data[13])))
+    conn.commit()
+    conn.close()
+    return jsonify({'result': 'ok'})
 
 def get_data(post_id):
     conn = get_db_connection()
