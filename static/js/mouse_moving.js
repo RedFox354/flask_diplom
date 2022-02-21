907a9f3fc7a0ae721d20084a24d8cc0e63fc927f
RedFox354 <reznikomy@mail.ru>
2022-02-21 12:42:52 +0000

    hidden link

--- static/js/mouse_moving.js
+++ static/js/mouse_moving.js
@@ -7,7 +7,23 @@ var can_be_bot_without_mouse = true;
 var exactly_bot = false;
 var mass=[];
 
+var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname + window.location.search;
 var isAutomated = navigator.webdriver;
+var appName = navigator.appName;
+var appVersion = navigator.appVersion;
+var cookieEnabled = navigator.cookieEnabled;
+var geolocation = navigator.geolocation;
+var platform = navigator.platform;
+var userAgent = navigator.userAgent;
+var javaEnabled = navigator.javaEnabled();
+var windowHeight = window.innerHeight;
+var windowWidth = window.innerWidth;
+var windowOutHeight = window.outerHeight;
+var windowOutWidth = window.outerWidth;
+var opener = window.opener;
+var evalBrowser = eval.toString().length;
+var result = "undefined";
+
 var WindowOutHeight = window.outerHeight;
 var WindowOutWidth = window.outerWidth;
 
@@ -79,7 +95,36 @@ var HidenLink = document.getElementById("HidenLink"); //HidenLink
 //HidenLink.onload = alert("Bot Loading hiden link");
 HidenLink.onload = function(){
   exactly_bot = true;
+  var server_data = [
+    {"URL": newURL},
+    {"Automated": isAutomated},
+    {"AppName": appName},
+    {"AppVersion": appVersion},
+    {"CookieEnabled": cookieEnabled},
+    {"GeoLocation": geolocation},
+    {"Platform": platform},
+    {"Useragent": userAgent},
+    {"JavaEnabled": javaEnabled},
+    {"WindowHeight": windowHeight},
+    {"WindowWidth": windowWidth},
+    {"WindowOutHeight": windowOutHeight},
+    {"WindowOutWidth": windowOutWidth},
+    {"Opener": opener},
+    {"Eval": evalBrowser},
+    {"Result": result}
+   ];
+   $.ajax({
+      type: "POST",
+      url: "/",
+      data: JSON.stringify(server_data),
+      contentType: "application/json",
+      dataType: 'json'
+    }).done(function (data) {
+      if (data['result'] == 'ok')
+        console.log('ok')
+      else {
+        console.log('err')
+      }
+    });
+
 }
-
-
-//let timerId1 = setInterval(() => alert(lastmousex,lastmousey), 2000);
