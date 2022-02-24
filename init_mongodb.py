f7a2c9d87106ef013cee8d80180cd6188b5ba13e
RedFox354 <reznikomy@mail.ru>
2022-02-24 13:20:29 +0000

    init_mongo_db

---
+++ init_mongodb.py
@@ -0,0 +1,12 @@
+import pymongo
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["mongo_database"]
+col = db["logs"]
+dict = {"url":"testurl","created":"testtime","isAutomated":"true","appName":"testapp",
+    "appVersion":"testver","cookieEnabled":"true","geolocation":"testloc",
+    "platform":"testplatform","userAgent":"testagent","javaEnabled":"true",
+    "Height":"1","Width":"1","OutHeight":"1","OutWidth":"1","Opener":"trueopener",
+    "evalBrowser":"111","result":"exactly_bot"}
+x = col.insert_one(dict)
+#insert_many([{},{}])
+#print(x.inserted_ids)
