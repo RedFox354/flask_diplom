1964d01f39b47a6ceb9a13ea17e0368accd41b87
RedFox354 <reznikomy@mail.ru>
2022-02-24 15:22:37 +0000

    finding and printing

--- init_mongodb.py
+++ init_mongodb.py
@@ -2,11 +2,13 @@ import pymongo
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["mongo_database"]
 col = db["logs"]
-dict = {"url":"testurl","created":"testtime","isAutomated":"true","appName":"testapp",
+dict = {"url":"testurl1","created":"testtime","isAutomated":"true","appName":"testapp",
     "appVersion":"testver","cookieEnabled":"true","geolocation":"testloc",
     "platform":"testplatform","userAgent":"testagent","javaEnabled":"true",
     "Height":"1","Width":"1","OutHeight":"1","OutWidth":"1","Opener":"trueopener",
     "evalBrowser":"111","result":"exactly_bot"}
-x = col.insert_one(dict)
+#x = col.insert_one(dict)
 #insert_many([{},{}])
 #print(x.inserted_ids)
+x = col.find_one()
+print(x)
